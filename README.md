2b844ff86c1ab6de7c29742eb14e25db85bced3f
rajinder <deol.rajinder@gmail.com>
2015-04-28 16:22:05 +0530

    Added dependencies and click events

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A simple application to scan 1D/2D barcodes in android using ZXing library
 Details of the entire Application Available on my Blog
 http://blog.whomeninja.in/android-barcode-scanner/
 
-Purposfully built using Android Studio and gradle
+Built using Android Studio with gradle
--- app/src/main/java/in/whomeninja/android_barcode_scanner/HomeActivity.java
+++ app/src/main/java/in/whomeninja/android_barcode_scanner/HomeActivity.java
@@ -1,17 +1,29 @@
 package in.whomeninja.android_barcode_scanner;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class HomeActivity extends ActionBarActivity {
+    private String codeFormat,codeContent;
+    private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
     }
 
 
@@ -35,5 +47,45 @@ public class HomeActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * event handler for scan button
+     * @param view view of the activity
+     */
+    public void scanNow(View view){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.setPrompt(String.valueOf(R.string.txt_scan_bar_code));
+        integrator.setResultDisplayDuration(0);
+        integrator.setWide();  // Wide scanning rectangle, may work better for 1D barcodes
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.initiateScan();
+    }
+
+
+    /**
+     * function handle scan result
+     * @param requestCode scanned code
+     * @param resultCode  result of scanned code
+     * @param intent intent
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanningResult != null) {
+            //we have a result
+            codeContent = scanningResult.getContents();
+            codeFormat = scanningResult.getFormatName();
+
+            // display it on screen
+            formatTxt.setText("FORMAT: " + codeFormat);
+            contentTxt.setText("CONTENT: " + codeContent);
+
+        }else{
+            Toast toast = Toast.makeText(getApplicationContext(),"No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
